ba3000cb98c84e96cbb9e2a2998abae5cc592101
nascimentoismael <ismaelazevedonascimento@gmail.com>
2021-03-14 16:19:15 -0300

    feat (CSS): mobile improvments

--- src/script.js
+++ src/script.js
@@ -1,4 +1,12 @@
 $(document).ready(() => {
+// menu
+    $('.navbar__menu-btn').on('click', function () {
+        $('.navbar__links').toggleClass('active');
+        $(this).find('i').toggleClass('fa-bars');
+        $(this).find('i').toggleClass('fa-times');
+    })
+
+//carousel
     const slickOptions = {
         autoplay: true,
         autoplaySpeed: 2000,
